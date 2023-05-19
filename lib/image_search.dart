e81395f168a3d332faf1d417e47cc83e50ca943c
Prashant Yadav <62587607+prashantprojection@users.noreply.github.com>
2023-05-19 16:11:51 +0530

    Update image_search.dart

--- lib/image_search.dart
+++ lib/image_search.dart
@@ -6,25 +6,10 @@ import 'package:http/http.dart' as http;
 class ImageSearch{
 final List<Image> _searchResults = [];
 
-/*
-  search(String query) async{
-var url =
-      Uri.https('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=$query&pageNumber=1&pageSize=10&autoCorrect=true');
-      var response = await http.get(url,headers: {
-        'X-RapidAPI-Key': 'a4224cc2eemshbdcba77917fd9c4p157f97jsn23bcb07e8d5a',
-    'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-      });
-      print(response);
-      if (response.statusCode==200) {
-        print("Successful");
-      }
-  }
-*/
-
  fetchSearchResults(String query) async {
   _searchResults.clear();
     final response = await http.get(Uri.parse(
-      'https://api.unsplash.com/search/photos?query=$query&client_id=j09zz6qA0Dg__RrTV8si7sjKJXdqPpQ-lV0Ptsjab5Y',
+      'https://api.unsplash.com/search/photos?query=$query&client_id=API-Key',
     ));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
